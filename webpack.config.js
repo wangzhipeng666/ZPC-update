ec8a8a697809246910235a524f31d310cb6fda92
wangzhipeng <1848993253@qq.com>
2023-05-16 08:36:05 +0800

    安装配置html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    bundleName: './src/index.js'
+    bundle: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // 打包后模板的名称
+      template: './src/index.html'  // 模板地址
+    })
+  ]
 }
