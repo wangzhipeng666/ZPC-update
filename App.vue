a153fa69bd2613294879f14f6b64fb39a06f402d
wangzhipeng <1848993253@qq.com>
2023-05-28 10:50:29 +0800

    vue项目准备工作

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">{{ message }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello world'
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ main.js
@@ -0,0 +1,6 @@
+import Vue form 'vue'
+import App form './App.vue'
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
